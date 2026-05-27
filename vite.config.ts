7f1912dd4dbe205b8d29e1990baa6e4e41cbafde
ASPCoding <152766272+ASPCoding@users.noreply.github.com>
2026-05-26 18:56:40 -0700

    Configure GitHub Pages deployment via GitHub Actions

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
+    base: '/CTC-Prompathon/',
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
